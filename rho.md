ae62f70647c1397e22a35fb0747cbda0a62b0a45
WomackCodes <chris@womack.codes>
2020-08-30 00:12:33 -0700

    Add .md file

---
+++ rho.md
@@ -0,0 +1,8 @@
+💲 - Money Sign Emoji
+
+### Steps taken to make this app: 
+
+1. Create a new directory named <app-name> and ensure there won't be any nested repository issues.
+
+2. 
+
--- server.js
+++ server.js
@@ -3,9 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// session middleware
+var session = require('express-session');
+var passport = require('passport');
+var methodOverride = require('method-override');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+// load the env vars
+require('dotenv').config();
 
 var app = express();
 
@@ -18,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
