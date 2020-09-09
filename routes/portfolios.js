b942be59d444f0e6da82cdf0bf0836c216e88a22
WomackCodes <chris@womack.codes>
2020-09-09 15:32:11 -0700

    Final changes

--- routes/portfolios.js
+++ routes/portfolios.js
@@ -4,10 +4,15 @@ const portfoliosCtrl = require('../controllers/portfolios');
 
 router.get('/', portfoliosCtrl.index);
 router.get('/new', portfoliosCtrl.new);
-router.get('/:id', portfoliosCtrl.show);
-router.post('/', portfoliosCtrl.create);
-router.delete('/:id', portfoliosCtrl.delete);
+router.get('/:id', isLoggedIn, portfoliosCtrl.show);
+router.post('/', isLoggedIn, portfoliosCtrl.create);
+router.delete('/:id', isLoggedIn, portfoliosCtrl.delete);
 // router.get('/:id/edit', portfolioCtrl.edit);
-router.put('/:id', portfoliosCtrl.update);
+router.put('/:id', isLoggedIn, portfoliosCtrl.update);
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect('/auth/google');
+}
 
 module.exports = router;
